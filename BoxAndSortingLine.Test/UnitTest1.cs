f377b69061384116923ab769d1472795b2756412
Mannuir <93124515+Mannuir@users.noreply.github.com>
2022-09-08 15:05:55 +0300

    test

---
+++ BoxAndSortingLine.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BoxAndSortingLine.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void When_ParcelHasNewDimensionsAndSortingLineHasNewDimensions_ThenParcelCanNotFitSortingLine()
+        {
+
+        }
+    }
+}
